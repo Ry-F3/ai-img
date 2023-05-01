f4f42d7d68937fdb22d8dc977256a65826da2d54
Ry-F3 <rhysfeew@gmail.com>
2023-05-01 09:45:47 +0000

    Fix - comment out unused JS file

--- assets/bin/javascript/menu.js
+++ assets/bin/javascript/menu.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let open = false;
+// document.addEventListener("DOMContentLoaded", function() {
+//     let open = false;
 
-    document.getElementById('hamburger').addEventListener('click', function (){
-        if (open){
-            open = false;
-            document.getElementById('menu-bar-mobile').classList.add('hide');
-        }
-        else{
-            open = true;
-            document.getElementById('menu-bar-mobile').classList.remove('hide');
-        }
+//     document.getElementById('hamburger').addEventListener('click', function (){
+//         if (open){
+//             open = false;
+//             document.getElementById('menu-bar-mobile').classList.add('hide');
+//         }
+//         else{
+//             open = true;
+//             document.getElementById('menu-bar-mobile').classList.remove('hide');
+//         }
 
-    });
+//     });
 
 
-});
+// });
 
 
 /* My mentor showed me how I would achieve the dropdown menu with JS */
